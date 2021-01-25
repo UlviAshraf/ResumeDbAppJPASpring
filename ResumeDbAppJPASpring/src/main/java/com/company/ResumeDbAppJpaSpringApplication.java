543789c737ba7801fa02f2f60d700c64eddb5bf8
UlviAshraf <ulvieshrefov002@gmail.com>
2021-01-25 19:22:56 +0400

    init

--- ResumeDbAppJPASpring/src/main/java/com/company/ResumeDbAppJpaSpringApplication.java
+++ ResumeDbAppJPASpring/src/main/java/com/company/ResumeDbAppJpaSpringApplication.java
@@ -18,6 +18,7 @@ public class ResumeDbAppJpaSpringApplication {
     private UserRepository userRepo;
 
     public static void main(String[] args) {
+
         SpringApplication.run(ResumeDbAppJpaSpringApplication.class, args);
     }
 
